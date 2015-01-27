c10ea31d9327952434065713bcaf0ccd8157c139
maxapryg <m.prytkova@sup.com>
2015-01-27 17:57:20 +0400

    Merge serg
    Conflicts:
    	src/main/java/com/livejournal/uitests/pages/journal_pages/EntryPage.java
    	src/test/java/com/livejournal/uitests/post_privacy/useful/users_post/UsersPost.java
    	src/test/resources/stories/livejournal/post_privacy/useful/users_post.story

--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.livejournal.uitests.db;
 
 import com.livejournal.uisteps.thucydides.WebTest;
@@ -17,12 +12,31 @@ public class DB extends WebTest {
     public void db() throws SQLException {
 
         System.out.println("!!!!!!!!!!!!! test start");
+        String select1 = "select u.user, u.userid, f.friendid from user u "
+                + "left join friends f on u.userid = f.userid "
+                + "where u.user = 'testautotest';";
+        List<ArrayList<String>> ans = this.workWithDB()
+                .conect()
+                .select(select1, "friendid")
+                .finish();
+        ArrayList<String> userid = ans.get(0);
 
-        String ans = getUserPassword("testautotest");
-        System.out.println(ans);
+        String select2 = "select user, userid from user "
+                + "where userid = '" + userid.get(0) + "' ";
+        for (int i = 1; i < userid.size(); i++) {
+            select2 = select2 + " or userid = '" + userid.get(i) + "'";
+        }
+        select2 = select2 + " and user like '%test%';";
+        System.out.println(select2);
 
-        //String password = this.getUserPassword("testautotest");
-        //System.out.println(password);
+        ArrayList<String> ans2 = this.workWithDB()
+                .conect()
+                .select(select2, "user")
+                .finish()
+                .get(0);
+        for (int i = 0; i < ans2.size(); i++) {
+            System.out.println(ans2.get(i));
+        }
         System.out.println("!!!!!!!!!!!!! test stop");
 
     }
--- src/test/resources/stories/livejournal/post_privacy/useful/post_in_community.story
+++ src/test/resources/stories/livejournal/post_privacy/useful/post_in_community.story
@@ -1,1 +1,15 @@
+Scenario: Create post in community
+Meta: 
+@categories feed privacy
 
+Given logged user <name> on Create Post page
+When user create new post with privacy <privacy> (group <group>)
+Then user <name_1> can read the post
+Then user <name_2> cannot read the post
+
+Examples:
+|name           |name_1         |name_2        |privacy     |group     | 
+|testautotest   |test           |              |Public      |          |
+|testautotest   |test_infriend  |test          |Friends     |          |
+|testautotest   |test_fgroup    |test          |Custom      |test_group|
+|testautotest   |testautotest   |test_fgroup   |Private     |          |
