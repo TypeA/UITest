dc5cd7a1e3e933aee91504d42b6cb37a1e0c91b6
maxapryg <m.prytkova@sup.com>
2015-08-24 15:08:07 +0400

    Merge dop
    Conflicts:
    	src/main/java/com/livejournal/uitests/pages/journal_pages/EntryPage.java
    	src/main/java/com/livejournal/uitests/pages/service_pages/update/UpdateBmlPageLogged.java
    	src/main/java/com/livejournal/uitests/pages/service_pages/update/visualEditor/HtmlsTags.java
    	src/test/java/com/livejournal/uitests/create_edit_post/html_tags/useful/CreatePostWithHtmlTags.java

--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -1,6 +1,9 @@
 package com.livejournal.uitests.db;
 
 import com.livejournal.uisteps.thucydides.WebTest;
+import com.livejournal.uitests.pages.service_pages.login_page.LoginPageUnlogged;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jbehave.core.annotations.Given;
 
 public class DB extends WebTest {
@@ -9,9 +12,13 @@ public class DB extends WebTest {
     public void db() {
 
         System.out.println("!!!!!!!!!!!!!!!!!!!! start");
-        System.out.println( "test " + this.getSystemConfiguration().getBaseUrl());
-        System.out.println("!!!!!!!!!!!!!!!!!!!! getUrlInPage");
-        System.out.println("!!!!!!!!!!!!! test stop");
+        this.openUrl("http://softlakecity.ru/browsers/firefox/kak-izmenit-user-agent-v-mozilla-firefox");
+        try {
+            Thread.sleep(300000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("!!!!!!!!!!!!!!!!!!!! finish");
 
     }
 
