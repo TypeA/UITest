9422fff0b7da7c660452056e6f227b114f77ab9f
maxapryg <m.prytkova@sup.com>
2015-11-10 17:31:39 +0400

    Merge maxa
    Conflicts:
    	thucydides.properties

--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -1,8 +1,16 @@
 package com.livejournal.uitests.db;
 
+import com.livejournal.uisteps.thucydides.ThucydidesUtils;
 import com.livejournal.uitests.LJTest;
+import com.livejournal.uitests.feed.personal.settings.Settings;
+import com.livejournal.uitests.pages.service_pages.friends_feed_pages.FriendsFeedLogged;
 import com.livejournal.uitests.pages.service_pages.login_page.LoginPageUnlogged;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jbehave.core.annotations.Given;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class DB extends LJTest {
 
@@ -17,7 +25,7 @@ public class DB extends LJTest {
 
 
         System.out.println("!!!!!!!!!!!!!!!!!!!! finish test");
-
-    }
+        
+    }  
 
 }
