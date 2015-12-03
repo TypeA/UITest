b48dd4b8d9c0f3a58bd9916cdc769bcc43050e8a
maxapryg <m.prytkova@sup.com>
2015-12-03 17:41:13 +0400

    Merge new_feed_page
    Conflicts:
    	src/test/java/com/livejournal/uitests/db/DB.java

--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -5,6 +5,7 @@ import com.livejournal.uitests.pages.service_pages.main_pages.MainPageUnlogged;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jbehave.core.annotations.Given;
 import org.openqa.selenium.logging.LogEntry;
 
@@ -29,34 +30,13 @@ public class DB extends LJTest {
             System.out.println(error);
         }
         
-        for (String error : warningerr) {
-            System.out.println(error);
-        }
+        
+     //   for (String error : warningerr) {
+       //     System.out.println(error);
+      //  }
 
-      //  ArrayList<String> errors = new ArrayList<String>();
-        //   ArrayList<String> severe = new ArrayList<String>();
-        //  ArrayList<String> warning = new ArrayList<String>();
-        //  for (LogEntry logEntry : getCurrentBrowser().getDriver().manage().logs().get("browser").getAll()) {
-        // if (logEntry.getLevel().toString().equals("SEVERE")) {
-        //          errors.add("[" + logEntry.getLevel() + "] " + logEntry.getMessage());
-          /*  }
-         if (logEntry.getLevel().toString().equals("WARNING")) {
-         warning.add("[" + logEntry.getLevel() + "] " + logEntry.getMessage());
-         }*/
-        //}
-        /*    System.out.println("!!!!!!!!!!!!!!!!!!!!");
-         for (LogEntry logEntry : getCurrentBrowser().getDriver().manage().logs().get("browser")
-         .filter(Level.WARNING)) {
-         System.out.println(logEntry);
-         }*/
-        //  for (int i = 0; i < errors.size(); i++) {
-        //      System.out.println(errors.get(i));
-        //  }
-     /*   System.out.println("!!!!!!!!!!!!!!!!!!!!");
-         for (int i = 0; i < warning.size(); i++) {
-         System.out.println(warning.get(i));
-         }*/
         System.out.println("!!!!!!!!!!!!!!!!!!!! finish test");
+
 
     }
 
