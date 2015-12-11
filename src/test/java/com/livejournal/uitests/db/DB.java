423c76deb9180a9282a7c9c5dc76e1b47c3dacd6
maxapryg <m.prytkova@sup.com>
2015-12-11 15:38:26 +0400

    Merge master into robot
    Conflicts:
    	thucydides.properties

--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -4,40 +4,70 @@ import com.livejournal.uitests.LJTest;
 import com.livejournal.uitests.pages.service_pages.main_pages.MainPageUnlogged;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
+import net.thucydides.core.annotations.StepGroup;
 import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
 import org.openqa.selenium.logging.LogEntry;
 
 public class DB extends LJTest {
 
     @Given("data from DB")
     public void db() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!! start test");
 
         open(MainPageUnlogged.class);
-        System.out.println("!!!!!!!!!!!!!!!!!!!! start test");
-        LinkedHashSet <String> severe = new LinkedHashSet <String>();
-        LinkedHashSet <String> warningerr = new LinkedHashSet <String>();
+
+    }
+
+    @Then("table is displayed")
+    public void table_is_displayed() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!! step 2");
+        ArrayList<String> loggs = getLoggs();
+
+        addTable().pageOpen("MainPageUnlogged")
+                .importantErrors(loggs.get(0))
+                .otherErrors(loggs.get(1))
+                .finish();
+        /* this.verify().that(false)
+         .ifResultIsExpected("Сообщение 1")
+         .ifElse("Сообщение 2")
+         .finish();*/
+        System.out.println("!!!!!!!!!!!!!!!!!!!! finish test");
+    }
+
+    @StepGroup
+    public ArrayList<String> getLoggs() {
+
+        LinkedHashSet<String> severe = new LinkedHashSet<String>();
+        LinkedHashSet<String> warningerr = new LinkedHashSet<String>();
         for (LogEntry logEntry : getCurrentBrowser().getDriver().manage().logs().get("browser").getAll()) {
             if (logEntry.getLevel().toString().equals("SEVERE")) {
-                severe.add(logEntry.getLevel() + " " + logEntry.getMessage());
+                severe.add(logEntry.getMessage());
             }
             if (logEntry.getLevel().toString().equals("WARNING")) {
-                warningerr.add(logEntry.getLevel() + " " + logEntry.getMessage());
+                warningerr.add(logEntry.getMessage());
             }
         }
+
+        String severe_logs = "";
+        String warningerr_logs = "";
+        int i = 1;
         for (String error : severe) {
-            System.out.println(error);
+            severe_logs += i + ". " + error + "\n";
+            i++;
         }
-        
-        
-     //   for (String error : warningerr) {
-       //     System.out.println(error);
-      //  }
+        i = 1;
+        for (String error : warningerr) {
+            warningerr_logs += i + ". " + error + "\n";
+            i++;
+        }
 
-        System.out.println("!!!!!!!!!!!!!!!!!!!! finish test");
+        ArrayList<String> ans = new ArrayList<String>();
+        ans.add(severe_logs);
+        ans.add(warningerr_logs);
 
-
+        return ans;
     }
 
 }
