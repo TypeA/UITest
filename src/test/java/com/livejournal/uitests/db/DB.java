6a6f6339dff6523cbc7ed6e924217d022985048d
maxapryg <m.prytkova@sup.com>
2015-12-14 15:21:24 +0400

    Merge master into robot
    Conflicts:
    	thucydides.properties

--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -23,51 +23,18 @@ public class DB extends LJTest {
     @Then("table is displayed")
     public void table_is_displayed() {
         System.out.println("!!!!!!!!!!!!!!!!!!!! step 2");
-        ArrayList<String> loggs = getLoggs();
+        List<ArrayList<String>> loggs = getLoggs();
 
         addTable().pageOpen("MainPageUnlogged")
                 .importantErrors(loggs.get(0))
-                .otherErrors(loggs.get(1))
+                .otherErrors(null)
+                .and()
+                .pageOpen("Page 2")
+                .importantErrors(loggs.get(1))
+                .otherErrors(loggs.get(0))
                 .finish();
-        /* this.verify().that(false)
-         .ifResultIsExpected("Сообщение 1")
-         .ifElse("Сообщение 2")
-         .finish();*/
+
         System.out.println("!!!!!!!!!!!!!!!!!!!! finish test");
-    }
-
-    @StepGroup
-    public ArrayList<String> getLoggs() {
-
-        LinkedHashSet<String> severe = new LinkedHashSet<String>();
-        LinkedHashSet<String> warningerr = new LinkedHashSet<String>();
-        for (LogEntry logEntry : getCurrentBrowser().getDriver().manage().logs().get("browser").getAll()) {
-            if (logEntry.getLevel().toString().equals("SEVERE")) {
-                severe.add(logEntry.getMessage());
-            }
-            if (logEntry.getLevel().toString().equals("WARNING")) {
-                warningerr.add(logEntry.getMessage());
-            }
-        }
-
-        String severe_logs = "";
-        String warningerr_logs = "";
-        int i = 1;
-        for (String error : severe) {
-            severe_logs += i + ". " + error + "\n";
-            i++;
-        }
-        i = 1;
-        for (String error : warningerr) {
-            warningerr_logs += i + ". " + error + "\n";
-            i++;
-        }
-
-        ArrayList<String> ans = new ArrayList<String>();
-        ans.add(severe_logs);
-        ans.add(warningerr_logs);
-
-        return ans;
     }
 
 }
