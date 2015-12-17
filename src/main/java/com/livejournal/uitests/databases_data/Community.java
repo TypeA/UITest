370d5bdf3251bd174f1d644dbf3b119b5e7c2ed1
maxapryg <m.prytkova@sup.com>
2015-12-17 14:06:51 +0400

    Merge master into robot
    Conflicts:
    	thucydides.properties

--- src/main/java/com/livejournal/uitests/databases_data/Community.java
+++ src/main/java/com/livejournal/uitests/databases_data/Community.java
@@ -1,7 +1,6 @@
 package com.livejournal.uitests.databases_data;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -11,15 +10,22 @@ import java.util.Random;
 public class Community extends DatabasesData {
 
     public String findMaintainerInComminuty(String community) {
-        ArrayList<String> users = targetIdWithParams(community, "A").get(0);
+        String select = "select r.targetid, r.type from user u left join reluser r on "
+                + "u.userid=r.userid where u.user = '"
+                + community + "' and r.type='A';";
+        ArrayList<String> users = workWithDB().conect()
+                .select(select, "targetid")
+                .finish()
+                .get(0);
         return users.get(new Random().nextInt(users.size()));
     }
 
-    public ArrayList<String> findMembersInCommunity(String community) {
-        String select = "select user from user where userid = "
-                + "(select userid from friends where friendid = "
-                + "(select userid as comm from user where user='" + community + "')and userid "
-                + "not in (select targetid from reluser where userid =friendid))";
+    public ArrayList<String> findAllMembersInCommunity(String community) {
+        String select = "select f.friendid, u.user from friends f"
+                + "left join user u"
+                + "on f.friendid = u.userid"
+                + "where f.userid = "
+                + "(select userid from user where user = '" + community + "');";
         return workWithDB().conect()
                 .select(select, "user")
                 .finish()
@@ -27,35 +33,17 @@ public class Community extends DatabasesData {
     }
 
     public String findMemberInCommunityNotInGroup(String community) {
-        String select = "select friendid from friends where userid="
-                + "(select userid from user where user='" + community + "') and "
-                + "friendid not in(select targetid from reluser where "
-                + "userid=(select userid from user where user='" + community + "')) and groupmask=1";
-        ArrayList<String> ans = workWithDB().conect()
-                .select(select, "friendid")
+        String select = "select f.userid, f.friendid, u.user, f.groupmask "
+                + "from friends f "
+                + "left join user u on f.friendid = u.userid "
+                + "where f.userid = "
+                + "(select userid from user where user = '" + community + "') "
+                + "and f.groupmask = 1;";
+        ArrayList<String> users = workWithDB().conect()
+                .select(select, "user")
                 .finish()
                 .get(0);
-        ArrayList<String> users = new ArrayList<String>();
-        for (String an : ans) {
-            String select2 = "Select user from user where userid = " + an;
-            String ans1 = workWithDB().conect()
-                    .select(select2, "user")
-                    .finish()
-                    .get(0)
-                    .get(0);
-            users.add(ans1);
-        }
         return users.get(new Random().nextInt(users.size()));
-    }
-
-    private List<ArrayList<String>>
-            targetIdWithParams(String community, String type) {
-        String select = "select r.targetid, r.type from user u left join reluser r on "
-                + "u.userid=r.userid where u.user = '"
-                + community + "' and r.type='" + type + "';";
-        return workWithDB().conect()
-                .select(select, "targetid")
-                .finish();
     }
 
 }
--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -1,14 +1,8 @@
 package com.livejournal.uitests.db;
 
 import com.livejournal.uitests.LJTest;
-import com.livejournal.uitests.pages.service_pages.main_pages.MainPageUnlogged;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import net.thucydides.core.annotations.StepGroup;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
-import org.openqa.selenium.logging.LogEntry;
 
 public class DB extends LJTest {
 
@@ -16,11 +10,11 @@ public class DB extends LJTest {
     public void db() {
         System.out.println("!!!!!!!!!!!!!!!!!!!! start test");
 
-         List<ArrayList<String>> ans = getDBDate()
-                 .community()
-                 .targetIdWithParams("test_comm", "A");
-         for (int i=0; i<ans.get(0).size(); i++){
-         System.out.println(ans.get(0).get(i) + " " + ans.get(1).get(i));}
+     //    List<ArrayList<String>> ans = getDBDate()
+            //     .community()
+            //     .targetIdWithParams("test_comm", "A");
+        // for (int i=0; i<ans.get(0).size(); i++){
+        // System.out.println(ans.get(0).get(i) + " " + ans.get(1).get(i));}
 
     }
 
