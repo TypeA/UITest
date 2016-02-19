1619d4ffb3ecd578173f2751f2a1306ffaba1896
maxapryg <m.prytkova@sup.com>
2016-02-19 16:20:19 +0400

    Merge master into robot
    Conflicts:
    	thucydides.properties

--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -1,6 +1,7 @@
 package com.livejournal.uitests.db;
 
 import com.livejournal.uitests.LJTest;
+import com.livejournal.uitests.pages.service_pages.login_page.LoginPageUnlogged;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 
@@ -9,7 +10,12 @@ public class DB extends LJTest {
     @Given("data from DB")
     public void db() {
         System.out.println("!!!!!!!!!!!!!!!!!!!! start test");
-
+       System.setProperty("webdriver.firefox.bin", "/usr/bin/firefox");
+       System.out.println(System.getProperty("webdriver.firefox.bin"));
+       
+       open(LoginPageUnlogged.class);
+       
+       
      //    List<ArrayList<String>> ans = getDBDate()
             //     .community()
             //     .targetIdWithParams("test_comm", "A");
