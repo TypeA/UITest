bb831dad43e4764b11852f3a2abcb58d41040674
maxapryg <m.prytkova@sup.com>
2016-09-15 18:16:56 +0400

    Merge master into pero
    Conflicts:
    	pom.xml
    	src/main/java/com/livejournal/uitests/pages/service_pages/profile/ProfilePageLogged.java
    	src/test/java/com/livejournal/uitests/db/DB.java
    	thucydides.properties

--- src/test/java/com/livejournal/uitests/db/DB.java
+++ src/test/java/com/livejournal/uitests/db/DB.java
@@ -1,26 +1,26 @@
 package com.livejournal.uitests.db;
 
 import com.livejournal.uitests.LJTest;
-import java.util.ArrayList;
+import com.livejournal.uitests.pages.service_pages.login_page.LoginPageUnlogged;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 
 public class DB extends LJTest {
-    
+
     @Given("data from DB")
     public void db() {
         System.out.println("START TEST +++++++++++++++++++++++++++++++");
-        ArrayList<String> ans = this.getDBDate().bannedUser().getAllUserInBannedList("testautotest");
-        for(int i=0; i<ans.size(); i++){
-            System.out.println(ans.get(i));
-        }
+        String name = "testautotest";
+        open(LoginPageUnlogged.class)
+                .authorizeBy(name, getDBDate().userData().getUserPassword(name))
+                .setDefault().defaultStyle(name);
         System.out.println("STOP TEST +++++++++++++++++++++++++++++++");
-        
+
     }
-    
+
     @Then("table is displayed")
     public void table_is_displayed() {
-        
+
     }
-    
+
 }
