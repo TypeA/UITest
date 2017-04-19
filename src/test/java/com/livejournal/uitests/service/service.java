f08a20c775dc0e4f9ea16a5065f09a8e2562918b
typea <sergsav88@gmail.com>
2017-04-19 11:53:14 +0300

    up_lj ssh command fixes for hostname. now it will be given from config

---
+++ src/test/java/com/livejournal/uitests/service/service.java
@@ -0,0 +1,14 @@
+package com.livejournal.uitests.service;
+
+import com.livejournal.uitests.LJTest;
+import org.jbehave.core.annotations.Given;
+
+/**
+ * Created by sergey.savinykh on 18.04.17.
+ */
+public class service extends LJTest {
+    @Given("user updating machine")
+    public void user_updating_machine() {
+        console().update_machine();
+    }
+}
