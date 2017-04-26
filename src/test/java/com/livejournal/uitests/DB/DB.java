f62718dfe29d5c4e3a19200637a8aabc94c41744
typea <sergsav88@gmail.com>
2017-04-26 13:24:17 +0300

    cleanup DB test

--- src/test/java/com/livejournal/uitests/DB/DB.java
+++ src/test/java/com/livejournal/uitests/DB/DB.java
@@ -2,9 +2,13 @@ package com.livejournal.uitests.DB;
 
 
 import com.livejournal.uitests.LJTest;
+import org.jbehave.core.annotations.Given;
 
 public class DB extends LJTest{
 
+    @Given("data from DB")
+    public void db() {
 
+    }
 
 }
